d7433a09479d01b5464a98ec473beeedfef77374
ftagulaw <ftagulaw@gmail.com>
2024-05-09 04:14:28 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<template>
+    <NuxtLayout name="default">
+    </NuxtLayout>
+</template>
+   
